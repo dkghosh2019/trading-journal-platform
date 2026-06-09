1f26ea4bf9849c1a1f209728d1f0943e5142d573
Dipak Ghosh <durja1008@gmail.com>
2026-06-09 14:28:45 -0400

    Initialize project structure and documentation

---
+++ trading-journal-api/src/main/java/com/dkghosh/tradingjournal/TradingJournalApiApplication.java
@@ -0,0 +1,13 @@
+package com.dkghosh.tradingjournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingJournalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingJournalApiApplication.class, args);
+	}
+
+}
